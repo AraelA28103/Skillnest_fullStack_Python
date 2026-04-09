e6b47c99e5b4b672b2412cede63943ac490118bd
Jheimy Tolentino <araelanabalon@liceovvh.cl>
2026-04-09 15:10:40 -0400

    09.04.26

--- python/fundamentos/fundamentos/02_estructuras_control/09_ejercicios_python.py
+++ python/fundamentos/fundamentos/02_estructuras_control/09_ejercicios_python.py
@@ -3,7 +3,6 @@ def paresDinamicos():
     pares = int(input("Cuantos números pares desea ver? "))
     for i in range(1, pares + 1):
         print(i * 2)
-paresDinamicos()
 
 
 
@@ -17,32 +16,62 @@ def verificadorEdad():
         print(f"Eres menor de edad, tienes {calculoEdad} anios")
     else:
         print("Error")
-verificadorEdad()
+
 
 
 # 3. Calculadora de Descuentos
 def calculadoraDescuentos():
     precioProducto = int(input("Ingrese el precio del producto: "))
-    cantidadComprada = int(input("Ingrese la cantidad de unidades compradas: "))
-    precioTotal = precioProducto * cantidadComprada
-    if (precioTotal > 100):
-        print()
-calculadoraDescuentos()
+    cantidad = int(input("Ingrese la cantidad de unidades compradas: "))
+    total = precioProducto * cantidad
+    descuento = 0
+    final = 0
+    if (total > 100):
+        descuento = total * (10 / 100)
+        final = total - descuento
+        print(f"Total: {total}\nDescuento: {descuento}\nFinal: {final}")
+
 
 
 # 4. Clasificador de Números
+def clasificadorNum():
+    num = int(input("Ingrese un número: "))
+    if (num > 0):
+        if (num % 2 == 0):
+            print(f"El número {num} es Positivo y Par")
+        elif (num % 2 == 1):
+            print(f"El número {num} es Positivo e Impar")
+    elif (num < 0):
+        if (num % 2 == 0):
+            print(f"El número {num} es Negativo y Par")
+        elif (num % 2 == 1):
+            print(f"El número {num} es Negativo e Impar")
 
 
 
 # 5. Tabla de Multiplicar Personalizada
+def tablaPresonalizada():
+    num = int(input("Ingresa un número: "))
+    for i in range(1, 13):
+        resultado = num * i
+        if (resultado % 3 == 0):
+            print(f"{num} * {i} = {resultado}")
 
 
 
 # 6. Sumatoria con Centinela
-
+def sumatoriaCentinela():
+    num = int(input("Ingresa un número: "))
+    suma = 0
+    while num != 0:
+        suma += num
+        num = int(input("Ingresa un número negativo para terminar"))
+    print(f"Total: {suma}")
 
 
 # 7. Contador de Vocales
+def contadorVocales():
+    palabra = input("Ingresa una palabra: ")
 
 
 
@@ -75,3 +104,48 @@ calculadoraDescuentos()
 
 
 # 15. Análisis de Temperaturas
+
+
+
+continuar = True
+while continuar:
+    print("\n---Ejercicios Python---")
+    print("---Ejercicio 1---")
+    print("---Ejercicio 2---")
+    print("---Ejercicio 3---")
+    print("---Ejercicio 4---")
+    print("---Ejercicio 5---")
+    print("---Ejercicio 6---")
+    print("---Ejercicio 7---")
+    print("---Ejercicio 8---")
+    print("---Ejercicio 9---")
+    print("---Ejercicio 10---")
+    print("---Ejercicio 11---")
+    print("---Ejercicio 12---")
+    print("---Ejercicio 13---")
+    print("---Ejercicio 14---")
+    print("---Ejercicio 15---")
+    opcion = input("\n---Elije una opción: (1-15) (0 para salir)")
+    if opcion == "1":
+        print("\nEjecutar ejercicio 1: ")
+        print(paresDinamicos())
+    elif opcion == "2":
+        print("\nEjecutar ejercicio 2: ")
+        print(verificadorEdad())
+    elif opcion == "3":
+        print("\nEjecutar ejercicio 3: ")
+        print(calculadoraDescuentos())
+    elif opcion == "4":
+        print("\nEjecutar ejercicio 4: ")
+        print(clasificadorNum())
+    elif opcion == "5":
+        print("\nEjecutar ejercicio 5: ")
+        print(tablaPresonalizada())
+    elif opcion == "6":
+        print("\nEjecutar ejercicio 5: ")
+        print(sumatoriaCentinela())
+    elif opcion == "0":
+        print("Saliendo...")
+        continuar = False
+    else:
+        print("Opción no válido. Intenta otra vez.")
